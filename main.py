13e3dd5bc8912384fb7af0c4969b4deae8c7efeb
theo <lesgooo1993@proton.me>
2025-03-18 19:44:56 +0530

    new custom slider

--- main.py
+++ main.py
@@ -38,9 +38,7 @@ curr_subtitle = tk.StringVar(value="---------")
 curr_time = tk.DoubleVar(value=0.0)
 curr_duration = tk.DoubleVar(value=100.0)
 
-curr_status = tk.StringVar(value="▶︎")
-
-scale = None
+curr_status = tk.StringVar(value="⏸")
 
 ######################################################################
 
@@ -59,25 +57,29 @@ def decrypt_url(url):
     player.set_state(Gst.State.PLAYING)
     is_playing = True
 
+
     bus = player.get_bus()
     bus.timed_pop_filtered(Gst.CLOCK_TIME_NONE, Gst.MessageType.EOS)
 
 ######################################################################
 
 def play_song(enc, title, subtitle):
+    # global scale
+    global start
+
     curr_title.set(title)
     curr_subtitle.set(subtitle)
     threading.Thread(target=decrypt_url, args=(enc,)).start()
-    continuously_update_time()
+    update()
 
-def continuously_update_time():
-    global scale
-
+def update():
     curr_time.set(player.query_position(Gst.Format.TIME)[1] / Gst.SECOND)
     curr_duration.set(player.query_duration(Gst.Format.TIME)[1] / Gst.SECOND)
-    scale.configure(to = curr_duration.get())
+    # scale.configure(to=curr_duration.get())
+
     start.configure(text=f"{(curr_time.get() / curr_duration.get()) * 100:.2f}%")
-    root.after(1000, continuously_update_time)
+    slider.configure(width=(curr_time.get() / curr_duration.get() * 800))
+    root.after(1000, update)
 
 ######################################################################
 
@@ -116,13 +118,12 @@ def search(event=None):
         img_url = song['image'].replace('150x150', '50x50')
         enc = song['more_info']['encrypted_media_url']
 
-        song_container = tk.Frame(results_frame, bg="black", width=800, height=50)
+        song_container = tk.Frame(results_frame, bg="black", width=850, height=50)
         song_container.pack_propagate(False)
 
         img_response = requests.get(img_url)
         img_data = img_response.content
         img = Image.open(BytesIO(img_data))
-        # img = img.resize((50, 50), Image.LANCZOS)
         photo = ImageTk.PhotoImage(img)
 
         song_img = tk.Label(song_container, image=photo, bg="black")
@@ -140,18 +141,18 @@ def search(event=None):
         song_subtitle.pack(anchor="w")
         song_details.pack(side=tk.LEFT)
 
-        highlight = tk.Frame(song_container, bg="black", width=10)
+        highlight = tk.Frame(song_container, bg="black", width=5)
         highlight.pack(fill=tk.Y, side="right")
 
         song_container.pack(pady=(0, 10))
         song_container.bind("<Button-1>", lambda event, enc=enc, title=title, subtitle=subtitle: play_song(enc, title, subtitle))
-        song_container.bind("<Enter>", lambda event, cont=highlight: cont.config(bg='dodgerblue'))
+        song_container.bind("<Enter>", lambda event, cont=highlight: cont.config(bg='red'))
         song_container.bind("<Leave>", lambda event, cont=highlight: cont.config(bg='black'))
 
         hr = tk.Frame(results_frame, bg="#292929", height=1)
         hr.pack(fill=tk.X, pady=(0, 10))
 
-    scrollbar.pack(side="right", fill="y", padx=(0, 10), pady=(10, 135))
+    scrollbar.pack(side="right", fill="y", padx=(0, 10), pady=(10, 125))
 
 ######################################################################
 
@@ -176,44 +177,61 @@ canvas.configure(yscrollcommand=scrollbar.set)
 canvas.bind_all("<Button-4>", lambda event: canvas.yview_scroll(-1, "units"))
 canvas.bind_all("<Button-5>", lambda event: canvas.yview_scroll(1, "units"))
 
-canvas.pack(side="left", fill="y", expand=True, pady=(10, 135))
+canvas.pack(side="left", fill="y", expand=True, pady=(10, 125))
 
 ######################################################################
 
 playbox = tk.Frame(root, bg="black", highlightbackground="#292929",
                    highlightcolor="#292929", highlightthickness=1, relief="groove")
-playbox.place(relx=0.5, rely=1.0, anchor='s', width=800, y=-20)
+playbox.place(relx=0.5, rely=1.0, anchor='s', width=800, y=-10)
+
+slider = tk.Frame(playbox, bg="red", height=4)
+slider.pack(side="top", anchor="w")
+
+def on_click(event): update_seekbar(event.x)
+def on_drag(event): update_seekbar(event.x)
+
+slider.bind("<Button-1>", on_click)
+slider.bind("<B1-Motion>", on_drag)
+
+def update_seekbar(x):
+    new_width = max(0, min(x, 800))
+    slider.configure(width=new_width)
+
+    newtime = ((new_width / 800) * curr_duration.get()) * Gst.SECOND
+    player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT, newtime)
 
 durbox = tk.Frame(playbox, bg="black")
 durbox.pack(fill="both", expand=True)
 
-start = tk.Label(durbox, text="0.0%", font=("GitLab Sans", 8), bg="black", fg="white")
+start = tk.Label(durbox, font=("GitLab Sans", 8), bg="black", fg="white")
 start.pack(side="left", padx=(5, 0))
 
-scale = ttk.Scale(durbox, variable=curr_time, from_=curr_time.get(), to=curr_duration.get(), orient=tk.HORIZONTAL)
-scale.pack(side="left", fill=tk.X, expand=True)
-
-def on_scale_change(event):
-    position_ns = curr_time.get() * Gst.SECOND
-    success = player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT, position_ns)
-
-scale.bind("<ButtonRelease-1>", on_scale_change)
+# scale = ttk.Scale(durbox, variable=curr_time, from_=curr_time.get(), to=curr_duration.get(), orient=tk.HORIZONTAL)
+# scale.pack(side="left", fill=tk.X, expand=True)
+#
+# def on_scale_change(event):
+#     position_ns = curr_time.get() * Gst.SECOND
+#     player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT, position_ns)
+#
+# scale.bind("<ButtonRelease-1>", on_scale_change)
 
 end = tk.Label(durbox, textvariable=curr_duration, font=("GitLab Sans", 8), bg="black", fg="white")
 end.pack(side="right", padx=(0, 5))
 
-######################################################################
+songdetbox = tk.Frame(playbox, bg="black")
+songdetbox.pack(fill="both", expand=True)
 
-title_label = tk.Label(playbox, textvariable=curr_title,
-                       font=("GitLab Sans", 12), bg="black", fg="white")
-title_label.pack()
+title_label = tk.Label(songdetbox, textvariable=curr_title,
+                       font=("GitLab Sans", 10), bg="black", fg="white")
+title_label.pack(side="left", padx=(5, 0))
 
-subtitle_label = tk.Label(playbox, textvariable=curr_subtitle,
+subtitle_label = tk.Label(songdetbox, textvariable=curr_subtitle,
                           font=("GitLab Sans", 10), bg="black", fg="white")
-subtitle_label.pack()
+subtitle_label.pack(side="right", padx=(0, 5))
 
 play_button = tk.Label(playbox, textvariable=curr_status, bg="black", fg="white", font=("monospace", 22))
-play_button.pack(side=tk.BOTTOM, pady=10)
+play_button.pack(side=tk.BOTTOM)
 play_button.bind("<Button-1>", lambda event: toggle_play())
 
 root.mainloop()
